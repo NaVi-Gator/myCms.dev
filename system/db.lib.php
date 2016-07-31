c38f56c742be52dcd762edc26744e29e4a86f0d7
NaVi-Gator <pro100vampir166@gmail.com>
2016-07-31 17:26:25 +0300

    dev_17:26 31.07.2016

--- system/db.lib.php
+++ system/db.lib.php
@@ -1,7 +1,8 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 @$DataBase = mysqli_connect(HOST, USER, PASS, DATABASE);
-
+$format_time = 'H:i d/m/Y';
+//Перевірка наявності даних адміна в базі
 function getAdminStatus($email, $pass)
 {
     global $DataBase;
@@ -17,21 +18,39 @@ function getAdminStatus($email, $pass)
         return false;
 }
 
+//створення нового поста
 function setPostToDataBase($title, $content)
 {
     global $DataBase;
     $title = $DataBase->escape_string($title);
     $content = $DataBase->escape_string($content);
     $time = $DataBase->escape_string(time());
-    $sql = "INSERT INTO `" . DATABASE . "`.`" . PR . "posts` (`id`, `title`, `content`, `create_time`) VALUES (NULL, '" . $title . "', '" . $content . "', '" . $time . "')";
+    $sql = "INSERT INTO `" . PR . "posts` (`id`, `title`, `content`, `create_time`) VALUES (NULL, '" . $title . "', '" . $content . "', '" . $time . "')";
     $DataBase->query($sql);
-    $res = $DataBase->query("SELECT id FROM `My__posts` WHERE create_time='" . $time . "'");
+    $res = $DataBase->query("SELECT id FROM `" . PR . "posts` WHERE create_time='" . $time . "'");
     $res->data_seek(0);
     $row = $res->fetch_assoc();
     if (strlen($row['id']) > 0)
         return $row['id'];
     else
         return '-1';
+}
+
+//Отримання списка постів
+function getPostsList($post_id_start = 0, $number = 1)
+{
+    global $DataBase, $format_time;
+    $post_array = array();
+    if ($number <= 0) $number = 1;
+    if ($post_id_start <= 0) $post_id_start = 1;
+    $sql = "SELECT * FROM `" . PR . "posts` WHERE id >= " . (int)$post_id_start . " LIMIT " . (int)$number;
+    $res = $DataBase->query($sql);
+    $res->data_seek(0);
+    while ($row = $res->fetch_assoc()) {
+        $row['create_time'] = date($format_time, (int)$row['create_time']);
+        array_push($post_array, $row);
+    }
+    return $post_array;
 }
 
 function showArray($array)
